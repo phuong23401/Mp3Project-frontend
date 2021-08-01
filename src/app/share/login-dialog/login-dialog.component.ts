422847dd4908fd8ef27246ff3e205ae1b9a6c7cf
phuong23401 <hoaphuong23401@gmail.com>
2021-08-01 15:35:27 +0700

    updated-login-register-profile

--- src/app/share/login-dialog/login-dialog.component.ts
+++ src/app/share/login-dialog/login-dialog.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/service/login/login.service';
 
+declare var Swal: any;
 @Component({
   selector: 'app-login-dialog',
   templateUrl: './login-dialog.component.html',
   styleUrls: ['./login-dialog.component.scss']
 })
 export class LoginDialogComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
 
-  constructor() { }
+  isLogin = false;
+
+  constructor(private router: Router,
+    private formBuilder: FormBuilder,
+    private loginService: LoginService) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    });
   }
 
+  login() {
+    const data = this.loginForm.value;
+    console.log(data);
+    this.loginService.login(data).subscribe(res => {
+      // tslint:disable-next-line:triple-equals
+      if (res.id != null) {
+        const jwt = res.token;
+        sessionStorage.setItem('token', JSON.stringify(jwt));
+        sessionStorage.setItem('userId', JSON.stringify(res.id));
+        sessionStorage.setItem('name',JSON.stringify(res.username));
+        this.router.navigate(['']);
+
+        // @ts-ignore
+       document.querySelector('.modal-backdrop').remove()
+       document.body.classList.remove('modal-open')
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Đăng nhập thất bại!',
+        });
+      }
+    });
+  }
 }
