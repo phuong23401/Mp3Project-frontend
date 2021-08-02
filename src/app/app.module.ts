6f1b4e0738b108f3f3277f6481aff85ff2e07be0
deepvn98 <deepvn988@gmail.com>
2021-08-03 01:07:58 +0700

    create_song done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,8 @@ import { RegisterDialogComponent } from './share/register-dialog/register-dialog
 import { Header1Component } from './share/headerr/header1/header1.component';
 import { ListNewSongComponent } from './song/list-new-song/list-new-song.component';
 import { TwoMostListenedComponent } from './song/two-most-listened/two-most-listened.component';
-
+import { ChangepasswordComponent} from "./profile/changepassword/changepassword.component";
+import {httpInterceptorProviders} from "./service/auth/auth.interceptor";
 
 @NgModule({
   declarations: [
@@ -57,6 +58,7 @@ import { TwoMostListenedComponent } from './song/two-most-listened/two-most-list
     Header1Component,
     ListNewSongComponent,
     TwoMostListenedComponent,
+    ChangepasswordComponent
   ],
   imports: [
     BrowserModule,
@@ -69,7 +71,7 @@ import { TwoMostListenedComponent } from './song/two-most-listened/two-most-list
     BrowserAnimationsModule,
     ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
