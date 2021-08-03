4025f0d84a534167dc200fb2497318cf69f01aac
ha271097 <nguyenquangha2710@gmail.com>
2021-08-03 15:46:40 +0700

    fix font

--- src/app/share/headerr/header1/header1.component.ts
+++ src/app/share/headerr/header1/header1.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { BsModalService } from 'ngx-bootstrap/modal';
+import {TokenService} from "../../../service/token/token.service";
 
 @Component({
   selector: 'app-header1',
@@ -10,10 +11,15 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 })
 export class Header1Component implements OnInit {
   searchForm: FormGroup;
+ nameuser: string;
 
   constructor(private modalService: BsModalService,
     private formBuilder: FormBuilder,
-    private router: Router) { }
+    private router: Router,
+              private tokenService: TokenService) {
+    this.nameuser = this.tokenService.getUsername();
+    console.log(this.nameuser);
+  }
 
   ngOnInit(): void {
   }
@@ -33,7 +39,7 @@ export class Header1Component implements OnInit {
   createSong() {
     this.router.navigate(['/createsong']);
   }
-  
+
   logout(){
     sessionStorage.removeItem('token');
     sessionStorage.removeItem('userId');
