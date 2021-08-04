96eea34e6dac54d2b52c7d5d7f3a3f50ccc524ad
deepvn98 <deepvn988@gmail.com>
2021-08-04 15:02:04 +0700

    show song all done

--- src/app/song/song-manager/song.component.ts
+++ src/app/song/song-manager/song.component.ts
@@ -8,29 +8,60 @@ import {Song} from "../../model/Song";
   styleUrls: ['./song.component.css']
 })
 export class SongComponent implements OnInit {
-id:any;
-audio:any;
-song:Song={};
-  listSong:Song[]=[];
-  constructor(private songService: SongService) {
-    this.songService.getAllSongs().subscribe((data)=>{
-      this.listSong = data;
+// id:any;
+// audio:any;
+// song:Song={};
+//   listSong:Song[]=[];
+//   constructor(private songService: SongService) {
+//     this.songService.getAllSongs().subscribe((data)=>{
+//       this.listSong = data;
+//     })
+//   }
+//
+//   ngOnInit(): void {
+//   }
+//   deleteSong(id:number){
+//
+//   }
+//   playSong(id:number){
+//       this.songService.getSongById(id).subscribe((data)=>{
+//         this.song = data;
+//         this.audio.src = this.song.fileUrl;
+//         this.audio.load();
+//         this.audio.play();
+//         console.log(this.song)
+//       })
+//   }
+
+
+  songList: Song[];
+  isPlaying = false;
+  audio : any;
+  song: Song;
+  constructor(private songService: SongService,
+  ) { }
+
+  ngOnInit(): void {
+    this.isPlaying = false;
+    this.songService.getAllSongs().subscribe(res =>{
+      this.songList = res;
     })
   }
 
-  ngOnInit(): void {
-  }
-  deleteSong(id:number){
+  listenCount(song:Song){
+    this.isPlaying = !this.isPlaying;
+    this.audio = new Audio();
+    this.audio.src = song.fileUrl;
+    this.audio.load();
+    this.audio.play();
+    this.songService.getListenSongById(song.id).subscribe(data=>{
+      this.song = data;
+    })
 
   }
-  playSong(id:number){
-      this.songService.getSongById(id).subscribe((data)=>{
-        this.song = data;
-        this.audio.src = this.song.fileUrl;
-        this.audio.load();
-        this.audio.play();
-        console.log(this.song)
-      })
+  changePause(){
+    this.isPlaying = !this.isPlaying;
+    this.audio.pause();
   }
 
 
