2d8bc49ad933380039eed85daaf70a90c116f622
phuong23401 <hoaphuong23401@gmail.com>
2021-08-06 11:54:09 +0700

    update playlist service

--- src/app/service/playlist/playlist.service.ts
+++ src/app/service/playlist/playlist.service.ts
@@ -1,9 +1,28 @@
+import { environment } from './../../../environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Playlist } from 'src/app/model/Playlist';
 
+const API_URL = `${environment.API_URL}`;
 @Injectable({
   providedIn: 'root'
 })
 export class PlaylistService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllPlaylist(): Observable<Playlist> { 
+    return this.httpClient.get<Playlist>(API_URL + "/playlist/getAll");
+  }
+
+  creatPlaylist(playlist: Playlist): Observable<Playlist> {
+    return this.httpClient.post<Playlist>(API_URL + "/playlist/create", playlist);
+  }
+
+  updatePlaylist(id: number, playlist: Playlist): Observable<Playlist> {
+    return this.httpClient.put<Playlist>(API_URL + "/playlist/edit" + id, playlist);
+  }
+
+
 }
