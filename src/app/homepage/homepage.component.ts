d195b24700ab736d3be859c0651ac50bc7ffa0ba
phuong23401 <hoaphuong23401@gmail.com>
2021-08-10 21:57:05 +0700

    fix css

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -47,17 +47,6 @@ export class HomepageComponent implements OnInit, OnDestroy {
       });
   }
 
-  likeCount(song: Song) {
-    this.songService.getLikeSongUpById(song.id).subscribe(
-      (data) => {
-        this.song = data;
-      },
-      (error) => {
-        alert('Please login before click like!');
-      }
-    );
-  }
-
   getPlayList() {
     this.playlist.getPlaylist(this.id).subscribe((data) => {
       this.lisplaylists = data;
